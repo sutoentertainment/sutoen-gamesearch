575c158f441670de8ee16387495eb4bfb3c7e2ef
Anh Duong <anh.duong.ninka@gmail.com>
2015-12-21 14:22:15 +0200

    Add test to check if INTERNET permission allowed

--- app/src/androidTest/java/com/sutoen/g2adeal/ApplicationTest.java
+++ app/src/androidTest/java/com/sutoen/g2adeal/ApplicationTest.java
@@ -1,16 +1,19 @@
 package com.sutoen.g2adeal;
 
+import android.Manifest;
 import android.app.Application;
 import android.test.ApplicationTestCase;
-import android.test.suitebuilder.annotation.SmallTest;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
     public ApplicationTest() {
         super(Application.class);
     }
+
+    private static final boolean DEBUG = true;
 
     @Override
     public void setUp() throws Exception {
@@ -22,8 +25,11 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super.tearDown();
     }
 
-    @SmallTest
-    public void testSomething() {
-        fail("Not implemented yet");
+
+    public void test_InternetPermission () {
+        final String PKG = "com.sutoen.snowball";
+        final String ACTIVITY =  PKG + ".MainActivity";
+        final String PERMISSION = Manifest.permission.INTERNET;
+        assertActivityRequiresPermission(PKG, ACTIVITY, PERMISSION);
     }
 }
